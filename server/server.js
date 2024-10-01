e9fbbc358d158dba130efa0a7cbcb207e1508a33
uzairraja-eng <rajauzair622@gmail.com>
2024-10-01 18:44:48 +0500

    udated fro netlifys

--- server/server.js
+++ server/server.js
@@ -214,9 +214,10 @@ app.get('/api/logs', protectRoute, async (req, res) => {
 });
 
 // Start the server
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
+console.log('Environment Variables:', process.env);
 
 // WebSocket setup
 const wss = new Server({ server }); // WebSocket Server
